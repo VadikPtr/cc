6cb66637e95dd90965d4037a539331bd0079b2f2
cprkv <mnz-rrh@ya.ru>
2025-09-06 04:09:25 +0500

    job stealing parallel for

--- cc/inc/cc/parallel.hpp
+++ cc/inc/cc/parallel.hpp
@@ -3,30 +3,44 @@
 #include "cc/threads.hpp"
 
 namespace details {
-  template <typename TData>
+  template <typename TValue>
   class ParallelForThread final : public ThreadFunc {
-    Arr<TData> data_;
-    void (*value_func_)(TData&);
-    void (*on_error)(TData&, const Err& err);
-    AtomicInt& failed_;
+   public:
+    struct Info {
+      ArrView<TValue> values;
+      Mutex           mutex;
+      size_t          taken_jobs = 0;
+      AtomicInt       failed     = 0;
+      void (*value_func)(TValue&);
+      void (*on_error)(TValue&, const Err& err);
+    };
+
+   private:
+    Info& info_;
 
    public:
-    ParallelForThread(Arr<TData> data, void (*value_func)(TData&),
-                      void (*on_error)(TData&, const Err& err), AtomicInt& failed)
-        : data_(move(data)),
-          value_func_(value_func),
-          on_error(on_error),
-          failed_(failed) {}
-
+    explicit ParallelForThread(Info& info) : info_(info) {}
     ~ParallelForThread() override = default;
 
     void run() override {
-      for (auto& value : data_) {
+      while (true) {
+        TValue* value = nullptr;
+
+        {
+          LockGuard lg{info_.mutex};
+          if (info_.taken_jobs == info_.values.size()) {
+            return;
+          }
+          value = &info_.values[info_.taken_jobs++];
+        }
+
         try {
-          value_func_(value);
+          info_.value_func(*value);
         } catch (const Err& err) {
-          if (on_error) on_error(value, err);
-          ++failed_;
+          if (info_.on_error) {
+            info_.on_error(*value, err);
+          }
+          ++info_.failed;
         }
       }
     }
@@ -37,19 +51,17 @@ template <typename TData>
 int parallel_for(const Arr<TData>& data, void (*func)(TData&),
                  void (*on_error)(TData&, const Err& err) = nullptr) {
   Arr<Thread> threads(Thread::hardware_thread_count() - 1);
-  size_t      current_thread = 0;
-  AtomicInt   failed_count   = 0;
+  typename details::ParallelForThread<TData>::Info info;
+  info.values     = data;
+  info.value_func = func;
+  info.on_error   = on_error;
 
-  // TODO: job stealing is better for this algo. but requires some syncro... :(
-  for (auto&& chunk : data.split_into_groups(threads.size())) {
-    auto* worker =
-        new details::ParallelForThread<TData>{move(chunk), func, on_error, failed_count};
-    threads[current_thread++].start(worker);
+  for (auto& thread : threads) {
+    thread.start(new details::ParallelForThread<TData>{info});
   }
-
   for (auto& thread : threads) {
     thread.join();
   }
 
-  return failed_count.load();
+  return info.failed;
 }
