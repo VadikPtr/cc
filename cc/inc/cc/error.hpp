6a847dedc7f1f20d4f371e69c5e35fb93707ca3e
Vadim Chuprakov <vadik@skbkontur.ru>
2025-09-13 17:32:22 +0500

    error thingy

--- cc/inc/cc/error.hpp
+++ cc/inc/cc/error.hpp
@@ -1,12 +1,14 @@
 #pragma once
 #include "cc/str.hpp"
 
+void dump_stacktrace();
+
 class Err : public std::exception {
   Str what_;
 
  public:
-  Err() : what_("Unknown error") {}
-  Err(Str what) : what_(move(what)) {}
+  Err() : Err(Str("Unknown error")) {}
+  Err(Str what) : what_(move(what)) { dump_stacktrace(); }
   ~Err() override = default;
 
   StrView message() const { return what_; }
