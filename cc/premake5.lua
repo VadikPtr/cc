cee861988cbccfe6b0fe8c4139fbea2bd4010b0f
Vadim Chuprakov <vadik@skbkontur.ru>
2025-10-06 12:08:20 +0500

    premake

---
+++ cc/premake5.lua
@@ -0,0 +1,15 @@
+---@diagnostic disable: undefined-field, undefined-global, lowercase-global
+
+local dir = os.getcwd()
+
+function link_cc()
+  includedirs { path.join(dir, "inc") }
+  links { "cc" }
+end
+
+project "cc"
+  kind "StaticLib"
+  language "C++"
+  buildoptions { warnings_flags, "-Wno-error=deprecated-declarations", }
+  files { "**.hpp", "**.cpp", "*.h", "*.c" }
+  includedirs { "inc", "src" }
