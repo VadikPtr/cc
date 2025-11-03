68a1844f747199d9587661934c6021f213da849d
Vadim Chuprakov <vadik@skbkontur.ru>
2025-11-04 01:33:01 +0500

    _sh

--- cc/inc/cc/str-view.hpp
+++ cc/inc/cc/str-view.hpp
@@ -92,3 +92,7 @@ class StrHash {
   bool operator==(const StrHash& o) const { return hash_ == o.hash_; }
   bool operator!=(const StrHash& o) const { return hash_ != o.hash_; }
 };
+
+inline StrHash operator""_sh(const char* cstr, size_t size) {
+  return StrHash(StrView(cstr, size));
+}
